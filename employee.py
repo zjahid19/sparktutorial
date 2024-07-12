6bcb085b18e5d0cea66a8f396ba9dc2051757e47
Jahid Sheikh <zjahid19@gmail.com>
2024-07-12 20:58:14 +0530

    before cool

--- employee.py
+++ employee.py
@@ -3,4 +3,5 @@ print('added for cherrypick')
 print('this is after cherrypick')
 print('this is meanwhile my_feature branch')
 print('this is my new feature branch methode-1')
+print('before cool')
 
